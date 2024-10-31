c82eb4a1b2902bb08dd10b7c53b58a9a5a18edb4
Hasanwirayuda1 <hasandaa.15@gmail.com>
2024-10-31 20:06:25 +0800

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,6 @@
+#buatlah program dengan output sebagai berikut
+print("Nama saya adalah [Hasan Wirayuda]")
+print("Nim saya adalah [24241133]")
+print("Saya berasal dari [Bebae,Dasan Baru]")
+print("Nomor keberuntungan saya adalah 24+33=57")
+print("TERIMAKASIH")
