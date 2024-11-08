981777769a379690537d4fb709d643de669f8a8f
Hasanwirayuda1 <hasandaa.15@gmail.com>
2024-11-08 21:10:56 +0800

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,17 @@
+x = 33 #integer,bilangan bulat
+print("tipe data dari x",type(x))
+
+y = 2.4 #float,bilangan
+print("tipe data dari y",type(y))
+
+f = "yuda ganteng" #string,kumpulan karakter
+print("tipe data dari f",type(f))
+
+a = True #boolean,hanya 2,yes or no
+print("tipe data dari a",type(a))
+
+b = "9.99999"
+print("tipe data dari b",type(b))
+
+
+
