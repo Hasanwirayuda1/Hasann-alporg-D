df279eddeb0f87be77557f455c0cc47ab6a87b04
Hasanwirayuda1 <hasandaa.15@gmail.com>
2024-12-15 14:10:13 +0800

    Add files via upload

---
+++ tugas9(24241133).py
@@ -0,0 +1,13 @@
+#nilai dalama suhu
+celcius = 33
+celcius = int(input('masukan suhu dalam celcius:'))
+print("nilai suhu adalah",celcius)
+#reamur
+reamur = (4/5) * celcius
+print("nilai suhu adalah",reamur,"reamur")
+#fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("nilai suhu adalah",fahrenheit,"fahrenheit")
+#kelvin
+kelvin = celcius + 273
+print("nilai suhu adalah",kelvin,"kelvin")
